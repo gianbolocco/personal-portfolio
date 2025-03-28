af709d8feb3c59ea40505d9ff82e2674e3695e0b
gianlucca bolocco <gianluccabolocco@gmail.com>
2025-03-28 01:42:21 -0300

    fix contacto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,36 +3,38 @@ import React from 'react'
 function Contact() {
   return (
     <div className='flex flex-col mb-10 mx-auto'>
+      <h1 className='text-center text-3xl'>Contacto</h1>
       <div className="flex justify-center items-center">
         <form
-          action='https://getform.io/slug'
+          action='https://getform.io/f/agdpzvob'
           method='POST'
-          className='flex flex-col w-full md:w-7/12'
+          className='flex mx-5 flex-col w-full md:w-7/12'
         >
           <input
             type='text'
             name='name'
             placeholder='Nombre'
-            className='p-2 bg-transparent border-1 rounded-md focus:outline-none'
+            required
+            className='p-2 mt-2 bg-transparent border-1 rounded-md focus:outline-none'
           />
           <input
             type='text'
             name='email'
             placeholder='Email'
+            required
             className='my-2 p-2 bg-transparent border-1 rounded-md focus:outline-none'
           />
           <textarea
             name='message'
             placeholder='Mensaje'
             rows='10'
+            required
             className='mb-4 p-2 bg-transparent border-1 rounded-md focus:outline-none'
-          >
-          {/* <button
-            type='button'
-            className='text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white'
-          > Enviar </button> */}
+          />
+          <button
+            type='submit'
+            className="cursor-pointer text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gray-700 drop-shadow-md hover:bg-gray-950 transition-all"> Enviar </button>
 
-          </textarea>
         </form>
       </div>
 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PortfolioItem from './PortfolioItem'
 import portfolio from '../data/portfolio'
 import { useTheme } from '../context/ThemeContext'
+import { div } from 'framer-motion/client'
 
 
 function Portfolio() {
@@ -9,16 +10,19 @@ function Portfolio() {
   const {darkMode} = useTheme()
 
   return (
-    <div className={`py-10 flex flex-col md:flex-row items-center justify-center ${ !darkMode ? "bg-gray-900 text-white " : "bg-white text-gray-900" } transition-all duration-700 ease-in-out`}>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {portfolio.map(project => (
-        <PortfolioItem 
-        imgUrl={project.imgUrl}
-        title={project.title}
-        stack={project.stack}
-        link={project.link}
-        />
-        ))}
+    <div className={`flex flex-col  ${ !darkMode ? "bg-gray-900 text-white " : "bg-white text-gray-900" } transition-all duration-700 ease-in-out`}>
+            <h1 className='text-center text-3xl'>Portfolio</h1>
+      <div className={`py-10 flex flex-col md:flex-row items-center justify-center transition-all duration-700 ease-in-out`}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {portfolio.map(project => (
+          <PortfolioItem 
+          imgUrl={project.imgUrl}
+          title={project.title}
+          stack={project.stack}
+          link={project.link}
+          />
+          ))}
+        </div>
       </div>
     </div>
   )
